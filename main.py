f43089fcc0bfe47f1d360337b1c95ca407ee53d7
guaguaguaxia <guaguaguaxia@Gmail.com>
2025-04-17 10:21:55 +0800

    feat:新增

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ def getAIExplain(API_KEY):
     # model = "gpt-4o"
     model = "grok-3"
     for i in f.readlines():
-        if k > 320:
+        if k > 318:
             break
         word = i.replace("\n", "")
         try:
