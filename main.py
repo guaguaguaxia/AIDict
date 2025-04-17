92b1ec53eed62f1977ccccd3dd316453c6ec2c48
guaguaguaxia <guaguaguaxia@Gmail.com>
2025-04-17 20:38:42 +0800

    feat:api

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ def getAIExplain(API_KEY):
     # model = "gpt-4o"
     model = "grok-3"
     for i in f.readlines():
-        if k > 10:
+        if k > 1:
             break
         word = i.replace("\n", "")
         try:
