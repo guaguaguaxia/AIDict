7c5f6fed5d108e497c1b6540e36c9ed1f02f5ae0
guaguaguaxia <guaguaguaxia@Gmail.com>
2025-04-18 17:22:52 +0800

    feat:前端

--- aidict-web/components/BreadcrumbNav/index.js
+++ aidict-web/components/BreadcrumbNav/index.js
@@ -34,12 +34,8 @@ export default function BreadcrumbNav({ word }) {
       };
     }
     
-    // 默认返回首页
-    return {
-      type: 'home',
-      text: '首页',
-      url: '/',
-    };
+    // 默认表示来自首页，返回null表示不显示中间面包屑
+    return null;
   };
   
   const referrer = getReferrer();
@@ -55,16 +51,21 @@ export default function BreadcrumbNav({ word }) {
             首页
           </Link>
         </li>
-        <li>
-          <div className="flex items-center">
-            <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path>
-            </svg>
-            <Link href={referrer.url} className="ml-1 text-gray-700 hover:text-blue-600 md:ml-2">
-              {referrer.text}
-            </Link>
-          </div>
-        </li>
+        
+        {/* 仅当有中间参考页面时显示中间面包屑 */}
+        {referrer && (
+          <li>
+            <div className="flex items-center">
+              <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path>
+              </svg>
+              <Link href={referrer.url} className="ml-1 text-gray-700 hover:text-blue-600 md:ml-2">
+                {referrer.text}
+              </Link>
+            </div>
+          </li>
+        )}
+        
         <li aria-current="page">
           <div className="flex items-center">
             <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
